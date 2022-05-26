0e69be19478197109eb7c04f555f943e6f19d1a1
Arthur-codeX <daniel.karanja239@gmail.com>
2022-05-26 09:21:34 +0300

    Finished DBS

--- DBS/book_store/admin.py
+++ DBS/book_store/admin.py
@@ -4,7 +4,8 @@ from .models import Book,Author
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     list_display=("title","rating", "author")
-
+    list_filter=("author","rating")
+    exclude=("rating",)
 
 admin.site.register(Book,BookAdmin)
 admin.site.register(Author)
