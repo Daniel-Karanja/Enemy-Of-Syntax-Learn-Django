08b1013e624541ef19092dd063336e86b6031356
Arthur-codeX <daniel.karanja239@gmail.com>
2022-06-08 16:23:58 +0300

    Changes

--- FORMS/forms/urls.py
+++ FORMS/forms/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns =[
     path("model_form",model_form,name='model_form'),
     path('class/model_form',Model_Form.as_view(),name='class_model_form'),
-    path('update/<int:id>',Update_User.as_view(),name='update_user')
+    path('update/<int:id>',update_user,name='update_user')
     ]
--- FORMS/forms/views.py
+++ FORMS/forms/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .forms import AuthorModelForm
 from django.views import View
 from bookstore.models import Author
@@ -19,12 +20,32 @@ class Model_Form(View):
         return render(request,'forms/class_model_form.html',{'form':form})
 
 
-class Update_User(View):
-    def get(self,request,*args,**kwargs):
-       user=Author.objects.get(pk=kwargs['id']) # Step Get the USer.
-       form=AuthorModelForm(instance=user)
-       return render(request,'forms/update.html',{'form':form})
-    
+# class Update_User(View):
+#     def get(self,request,**kwargs):
+#        print(kwargs['id'])
+#     #    user=Author.objects.get(pk=kwargs['id']) # Step Get the USer.
+
+#        form=AuthorModelForm()
+#        return render(request,'forms/update.html',{'form':form})
+
+
+def update_user(request,id):
+    #  user=Author.objects.get(pk=id)
+    #  form=AuthorModelForm(instance=user)
+    #  return render(request,'forms/update.html',{'form':form})
+    user=Author.objects.get(pk=id)
+    print(request.method)
+    if request.method == 'POST':
+        form=AuthorModelForm(request.POST,instance=user)
+        if form.is_valid():
+            form.save()
+            user=Author.objects.get(pk=id)
+            return render(request,'forms/update.html',{'form':form,'id':id})
+
+    print(user.name)
+    form=AuthorModelForm(instance=user)
+    return render(request,'forms/update.html',{'form':form,'id':id})
+    # return HttpResponse("Update user")
 
 def model_form(request):
     print(request.method)
